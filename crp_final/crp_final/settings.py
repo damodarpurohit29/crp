7e6685cb1fe2e32821702c747cd5c61951b126f3
Damodar <damodarpurohit29@gmail.com>
2025-05-16 23:39:10 +0530

    update commit1

--- crp_final/crp_final/settings.py
+++ crp_final/crp_final/settings.py
@@ -114,7 +114,6 @@ WSGI_APPLICATION = 'crp_final.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
